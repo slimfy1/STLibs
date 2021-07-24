c94d8a6bb4d545f5e9888448e9ec26e5916f6341
Andry <slimfy1@gmail.com>
2021-07-25 02:06:01 +0300

    Exampe init and usage

---
+++ examples/demo/Debug/Core/Lib/STLibs/PWM/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Lib/STLibs/PWM/pwm.c 
+
+OBJS += \
+./Core/Lib/STLibs/PWM/pwm.o 
+
+C_DEPS += \
+./Core/Lib/STLibs/PWM/pwm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Lib/STLibs/PWM/%.o: ../Core/Lib/STLibs/PWM/%.c Core/Lib/STLibs/PWM/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I"C:/Users/slimf/STM32CubeIDE/workspace_1.7.0/ServoTest/Core/Lib/STLibs" -I"C:/Users/slimf/STM32CubeIDE/workspace_1.7.0/ServoTest/Core/Lib" -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ examples/demo/Debug/Core/Lib/STLibs/Servo/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Lib/STLibs/Servo/Servo.c 
+
+OBJS += \
+./Core/Lib/STLibs/Servo/Servo.o 
+
+C_DEPS += \
+./Core/Lib/STLibs/Servo/Servo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Lib/STLibs/Servo/%.o: ../Core/Lib/STLibs/Servo/%.c Core/Lib/STLibs/Servo/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I"C:/Users/slimf/STM32CubeIDE/workspace_1.7.0/ServoTest/Core/Lib/STLibs" -I"C:/Users/slimf/STM32CubeIDE/workspace_1.7.0/ServoTest/Core/Lib" -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
